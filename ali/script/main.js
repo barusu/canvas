f66d6bfc0e2525ee65b5aa7e8a18b74e4a92c1e0
shiro <sunny@yui.org.cn>
2018-11-02 17:52:39 +0800

    update

---
+++ ali/script/main.js
@@ -0,0 +1,88 @@
+/**
+ * Ali
+ * 2018-11-01
+ */
+(function(scope) {
+  const json = [
+    {
+      id: '1',
+      icon: 'icon-circlefill',
+      label: '测试条目1',
+      children: [
+        {
+          id: '11',
+          label: '测试条目1-1',
+          children: [
+            {
+              id: '111',
+              label: '测试条目1-1-1',
+              action: ''
+            }
+          ]
+        }
+      ]
+    }, {
+      id: '2',
+      icon: 'icon-emojiflashfill',
+      label: '测试条目2',
+      children: [
+        {
+          id: '21',
+          label: '测试条目2-1',
+          children: [
+            {
+              id: '211',
+              label: '测试条目2-1-1',
+              action: ''
+            }
+          ]
+        }
+      ]
+    }
+  ];
+  var store = {};
+  var Sidebar = new Vue({
+    el: '#sidebar',
+    data: {
+      ich: json,
+      mindex: -1,
+      nindex: -1
+    },
+    methods: {
+      selectmain(index) {
+        this.mindex = index;
+        this.nindex = -1;
+      },
+      selectnav(index) {
+        this.nindex = index;
+      },
+      close() {
+        this.mindex = -1;
+        this.nindex = -1;
+      }
+    },
+    computed: {
+      ni() {
+        if(this.mindex !== -1 && this.ich[this.mindex]) {
+          if(Array.isArray(this.ich[this.mindex].children)) {
+            return this.ich[this.mindex].children;
+          }else {
+            return [];
+          }
+        }
+        return [];
+      },
+      san() {
+        if(this.nindex !== -1 && this.ni[this.nindex]) {
+          if(Array.isArray(this.ni[this.nindex].children)) {
+            return this.ni[this.nindex].children;
+          }else {
+            return [];
+          }
+        }
+        return [];
+      }
+    }
+  });
+})(window);
+
